3c44107452dd3bd4f68c92ff2afffb86cc1dde42
Rishit129 <89757697+Rishit129@users.noreply.github.com>
2022-04-10 14:06:23 -0400

    Add files via upload

--- quiz_game.js
+++ quiz_game.js
@@ -10,8 +10,8 @@ document.getElementById("player2_name").innerHTML = player2_name + ":";
 document.getElementById("player1_score").innerHTML = player1_score;
 document.getElementById("player2_score").innerHTML = player2_score;
 
-document.getElementById("player_question").innerHTML = "Question Turn - " + player1_name; 
-document.getElementById("player_answer").innerHTML = "Answer Turn - " + player2_name; 
+document.getElementById("player_question").innerHTML = "Question Turn -  " +  player1_name; 
+document.getElementById("player_answer").innerHTML = "Answer Turn -  " +  player2_name; 
 
 function send(){
     number1 = document.getElementById("number_one").value;
@@ -19,7 +19,7 @@ function send(){
 word = parseInt(number1)*parseInt(number2)
     question = "<h4 id ='word_display'> Q.  " + number1 + "x" + number2 + "</h4>";
     answer = "<br> Answer: <input type='text' id = 'cake'>";
-    check_button = "<br> <br> <button class ='btn btn-info' onclick='check()'> Check</button>";
+    check_button = "<br> <br> <button id='button_check' class ='btn btn-info' onclick='check()'> Check</button>";
     Row = question + answer + check_button;
 document.getElementById("output").innerHTML= Row;
 document.getElementById("word").value = "";
